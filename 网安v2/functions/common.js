4a65aa7d665d70b3e6457473bec192cb34631e7c
MROU <7915156722qq.com>
2013-05-11 19:25:22 +0800

    network security
    网络安全

---
+++ 网安v2/functions/common.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+$(".flip").click(function(){
+    $(".panel").slideToggle("slow");
+  });
+  $(".flip1").click(function(){
+    $(".panel1").slideToggle("slow");
+  });
+   $(".flip2").click(function(){
+    $(".panel2").slideToggle("slow");
+  });
+    $(".flip3").click(function(){
+    $(".panel3").slideToggle("slow");
+  });
+    $(".flip4").click(function(){
+    $(".panel4").slideToggle("slow");
+  });
+     $(".flip5").click(function(){
+    $(".panel5").slideToggle("slow");
+  });
+     $(".flip6").click(function(){
+    $(".panel6").slideToggle("slow");
+  });
+     $(".flip7").click(function(){
+    $(".panel7").slideToggle("slow");
+  });
+     $(".flip8").click(function(){
+    $(".panel8").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+
+
+$(".kcjs").click(function(){
+	  
+	  
+	  $(".js").slideToggle("slow");
+	  
+	});
+	
+    $(".kcsy").click(function(){
+    $(".sy").slideToggle("slow");
+  });
+  
+  $(".ktlx").click(function()
+  {
+	  
+	$(".lx").slideToggle("slow");  
+  }
+  
+  
+  );
+
+  
+});
